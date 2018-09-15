6800335d1bad1b6601c88240dae5780f2980272d
Ben Kelly <benjaminkelly@me.com>
2018-09-15 16:26:56 -0400

    Add files via upload

---
+++ Assets/Scripts/RingManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingManager : MonoBehaviour {
+    private float scale;
+    public GameObject ring;
+    public int numRings = 5;
+    public float ringBuffer = 2.0f;
+    public Vector2 center;
+    // Use this for initialization
+    void Start () {
+        scale = ringBuffer / 1.89332084f;
+        for(int i = 1; i <= numRings; i++)
+        {
+            GameObject newRing = Instantiate(ring);
+            newRing.transform.localScale = new Vector3(scale*i, scale*i, scale*i);
+            newRing.transform.position = center;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
