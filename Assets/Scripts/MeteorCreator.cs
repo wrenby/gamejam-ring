b0f053015249b9dfae8ade14cc0731a643b5877e
Audrey Baxter <baxtea@rpi.edu>
2018-09-16 13:30:05 -0400

    Balanced meteors

--- Assets/Scripts/MeteorCreator.cs
+++ Assets/Scripts/MeteorCreator.cs
@@ -7,7 +7,7 @@ public class MeteorCreator : MonoBehaviour {
     public GameObject meteor;
     private float startTime, lastCreation, lastFrequency, delay;
     private RingManager ringMan;
-    public int rate = 100, damageRate = 50;
+    public int hpRate = 750, damageRate = 50;
     public float lowScale = 0.3f, highScale = 1.0f;
 	// Use this for initialization
 	void Start () {
@@ -33,7 +33,7 @@ public class MeteorCreator : MonoBehaviour {
             newMeteor.transform.localScale = new Vector3(newScale, newScale, newScale);
             newMeteor.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, Mathf.PI * 2));
             Meteor newMeteorScript = newMeteor.GetComponent<Meteor>();
-            newMeteorScript.setMaxHP((int)((newScale)*rate * ((deltaTime/60 + 5))));
+            newMeteorScript.setMaxHP((int)((newScale + deltaTime/60)*hpRate));
             newMeteorScript.setDamage((int)((1 + newScale)*damageRate * ((deltaTime / 60 + 1))));
             lastCreation = Time.time;
         }
